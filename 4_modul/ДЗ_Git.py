04a4f73c75b88c44743c1641acc8a50be0f5a2d7
Andrey78965 <andreyvpi22@gmail.com>
2023-06-20 14:28:12 +0300

    '1.2'

--- 4_modul/ДЗ_Git.py
+++ 4_modul/ДЗ_Git.py
@@ -1,9 +1,9 @@
-str = 'лепсспел'.lower()
 def order(o):
    i = ''.join(reversed(o))
    if i == str:
     print(True)
    else:
     print(False)
-order(str)
+order('лепсспел')
+order('helloworld')
 
