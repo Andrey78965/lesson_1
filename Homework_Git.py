1eeb804724faacdb675a56c09be9cb6ca4ebfd8b
Andrey78965 <andreyvpi22@gmail.com>
2023-06-22 11:12:55 +0300

    '1.2'

--- Homework_Git.py
+++ Homework_Git.py
@@ -1,8 +1,15 @@
 def order(o):
-   i = ''.join(reversed(o))
-   if i == str:
-    print(True)
-   else:
-    print(False)
+    '''Функция проверяет является ли строка полинлромом или нет.
+
+    :param o: str
+    :return: возвращает True, если строка является палиндромом и False, если строка палиндромом не является.
+    '''
+    i = ''.join(reversed(o))
+    if i == o:
+        print(True)
+    else:
+        print(False)
 order('лепсспел')
 order('helloworld')
+print(order.__doc__)
+
