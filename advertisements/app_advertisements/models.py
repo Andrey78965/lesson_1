06f20f148fe0e789724cae991b27b48ba1b2b103
Andrey78965 <andreyvpi22@gmail.com>
2023-08-21 13:01:16 +0300

    'задание4_урок9'

--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.contrib import admin
 from django.utils.html import format_html
 from django.utils import timezone
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class Advertisement(models.Model):
     title = models.CharField("Заголовок", max_length=128)
@@ -10,7 +13,8 @@ class Advertisement(models.Model):
     auction = models.BooleanField("Торг", help_text='Отметьте, если торг уместен')
     updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
-
+    user = models.ForeignKey(User, verbose_name='пользователь', on_delete=models.CASCADE, null = True, blank = True)
+    image = models.ImageField('изображение', upload_to='advertisements/')
 
     @admin.display(description='дата создания')
     def created_date(self):
@@ -32,8 +36,15 @@ class Advertisement(models.Model):
     def __str__(self):
         return f"Advertisement(id={self.id}, title={self.title}, price={self.price})"
 
+    @admin.display(description='изображение')
+    def image_1(self):
+        if self.image:
+         return format_html(
+             '<img src="{}" style="max-height: 200px; max-width: 200px;">', self.image.url
+         )
+
     class Meta:
         db_table = "advertisements"
-
-
+'''http://127.0.0.1:8000/admin/app_advertisements/advertisement/advertisements.media
+http://127.0.0.1:8000/advertisements/%D0%A1%D0%BD%D0%B8%D0%BC%D0%BE%D0%BA_%D1%8D%D0%BA%D1%80%D0%B0%D0%BD%D0%B0_2023-08-14_161438_VQUALDO.png'''
 
